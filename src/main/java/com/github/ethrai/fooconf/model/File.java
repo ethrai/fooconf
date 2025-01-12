6908ece9648505b85c714b734867aacb82bcf60c
Sergei Ryzhko <ryzhkosergej@gmail.com>
2025-01-12 17:52:31 +0300

    feat(persistance): add initial models impl

--- src/main/java/com/github/ethrai/fooconf/model/File.java
+++ src/main/java/com/github/ethrai/fooconf/model/File.java
@@ -1,4 +1,17 @@
 package com.github.ethrai.fooconf.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "message_id", nullable = false)
+    private Message message;
 }
--- src/main/java/com/github/ethrai/fooconf/model/Poll.java
+++ src/main/java/com/github/ethrai/fooconf/model/Poll.java
@@ -1,4 +1,17 @@
 package com.github.ethrai.fooconf.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Poll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "poll_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "conference_id", nullable = false)
+    private Conference conference;
 }
