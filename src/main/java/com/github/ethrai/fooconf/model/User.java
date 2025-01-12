86b405de4b004d59378f52dd85c08682846d4aaf
Sergei Ryzhko <ryzhkosergej@gmail.com>
2025-01-12 21:54:31 +0300

    feat(models): add roles for user and participant

--- src/main/java/com/github/ethrai/fooconf/model/User.java
+++ src/main/java/com/github/ethrai/fooconf/model/User.java
@@ -1,16 +1,22 @@
 package com.github.ethrai.fooconf.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.sql.Date;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Data
 @Table(name = "\"user\"")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -27,10 +33,15 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private UserRole role;
+
     @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
     Set<Participant> conferencesParticipation;
 
     @CreationTimestamp
@@ -39,4 +50,20 @@ public class User {
 
     @UpdateTimestamp
     private Date updatedAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/github/ethrai/fooconf/model/UserRole.java
+++ src/main/java/com/github/ethrai/fooconf/model/UserRole.java
@@ -4,43 +4,22 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
 @AllArgsConstructor
-public class Participant {
+@NoArgsConstructor
+public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "participant_id")
+    @Column(name = "user_role_id", nullable = false)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "conference_id")
-    private Conference conference;
-
-    @OneToMany(mappedBy = "sender")
-    @ToString.Exclude
-    private Set<Message> messages;
-
-    @ManyToOne
-    @JoinColumn(name = "participant_role", nullable = false)
-    private ParticipantRole role;
-
-    private LocalDateTime invitedAt;
-
-    private LocalDateTime joinedAt;
-
-    private LocalDateTime leftAt;
+    @Column(nullable = false, unique = true)
+    private String name;
 
     @Override
     public final boolean equals(Object o) {
@@ -49,7 +28,7 @@ public class Participant {
         Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
         Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
-        Participant that = (Participant) o;
+        UserRole that = (UserRole) o;
         return getId() != null && Objects.equals(getId(), that.getId());
     }
 
